9a69f4903b41a3ff44760de23197884fdc1a6ea0
SilveryX <67977075+SilveryX@users.noreply.github.com>
2026-04-18 07:55:48 +0800

    Comment out default IP and hostname changes
    Comment out IP and hostname modifications in the script.

--- X86_64/diy-part2.sh
+++ X86_64/diy-part2.sh
@@ -11,11 +11,11 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 #sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
